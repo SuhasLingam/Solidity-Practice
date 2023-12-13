d842a672da83916c3240480118b742e385be109b
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2023-12-13 22:08:38 +0530

    placeBid

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,34 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+contract Auction {
+    address payable Auctioneer;
+    address[] users;
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
-
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    constructor() {
+        Auctioneer = payable(msg.sender);
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    modifier AuctioneerCannotBid() {
+        require(msg.sender != Auctioneer, "The auctioneer cannot bid");
+        _;
+    }
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function placeBid() public payable AuctioneerCannotBid {
+        require(msg.value > 0, "Need more ether");
+        uint HighestBid = 0;
+        if (msg.value > HighestBid) {}
     }
 }
