a90a3109d34102bc0aa65a35febd1c2302945e1c
SuhasLingam <95020208+SuhasLingam@users.noreply.github.com>
2023-12-19 01:54:28 +0530

    Contract Completed and Deployed in Sepolia Testnet :white_check_mark:

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// Go to https://alchemy.com, sign up, create a new App in
+// its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "xBo7c9nK2wg5LexoeEalUs9v7EjT4KT4";
+
+// Replace this private key with your Sepolia account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const SEPOLIA_PRIVATE_KEY =
+  "6dab07c8779d8fcbe5d4b92d9987b5c99912eaf05afa92c8e7f62162ad5e298b";
+
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ const hre = require("hardhat");
 
 async function main() {
   const Auction = await hre.ethers.getContractFactory("Auction");
-  const auction = await Auction.deploy();
+  const auction = await Auction.deploy(60); // give parameter for Constructor
 
   await auction.waitForDeployment();
 
@@ -13,3 +13,6 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+//contract address : 0xa9Bfd852a6dfecDD4c06CAD02D7425510e3A37b8
+// endTime: 60
